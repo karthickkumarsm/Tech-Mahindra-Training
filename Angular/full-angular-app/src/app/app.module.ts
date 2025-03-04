e649a7e546d4cf6d069fa411af03c6a4f551aa92
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2025-03-04 16:34:50 +0530

    Angular - 4th  march

--- Angular/full-angular-app/src/app/app.module.ts
+++ Angular/full-angular-app/src/app/app.module.ts
@@ -1,30 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProjectComponent } from './pages/project/project.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { MenuBarComponent } from './common/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProjectComponent,
-    ContactsComponent,
+    ProjectsComponent,
     AboutComponent,
-    ProjectsComponent
+    ContactsComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
