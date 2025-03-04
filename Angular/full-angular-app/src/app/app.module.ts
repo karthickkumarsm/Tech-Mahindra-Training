d644038e744c95e2e776f5fb93c764267eeb308c
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2025-03-04 15:04:48 +0530

    Angular - 4th  march

---
+++ Angular/full-angular-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProjectComponent,
+    ContactsComponent,
+    AboutComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
