5bd9186e34f361e8229d103744e2a91be4a7b1d7
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2025-03-12 14:57:46 +0530

    Flutter - March 12

--- Flutter/demo/lib/main.dart
+++ Flutter/demo/lib/main.dart
@@ -3,9 +3,8 @@ import 'pages/first_page.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/second_page.dart';
-
 import 'pages/third_page.dart';
-
+import 'pages/fourth_page.dart';
 void main() {
   runApp(MyApp());
 }
@@ -14,7 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+        title: 'Flutter Mobile App',
+        theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.orange),
+        textTheme: TextTheme(
+          headlineSmall: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          bodyLarge: TextStyle(fontSize: 16.0),
+          labelLarge: TextStyle(fontSize: 18.0, color: Colors.white),
+        ),
+      ),
       home: HomePage(),
     );
   }
@@ -28,54 +37,61 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.green,
-                elevation: 5,
-                shadowColor: Colors.green
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FirstPage()),
+              );
+            },
+            child: Text('Go to First Page'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondPage()),
+              );
+            },
+            child: Text('Go to Second Page'),
+          ),
+             ElevatedButton.icon(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ThirdPage()),
+              );
+            },
+            icon: Icon(Icons.navigate_next),
+            label: Text('Go to Third Page'),
+          )
+      ,
+      GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FourthPage()),
+              );
+            },
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(8.0),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FirstPage()),
-                );
-              },
-              child: Text('Go to First Page'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
-                );
-              },
-              child: Text('Go to Second Page'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                elevation: 5,
-                shadowColor: Colors.red
+              child: Text(
+                'Go to Fourth Page',
+                style: TextStyle(color: Colors.white),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ThirdPage()),
-                );
-              },
-              child: Text('Go to Third Page'),
             ),
-           
-          ],
-        ),
-      ),
+          )
+        ]
+      )
      
     );
+    
   }
 }
--- Flutter/demo/lib/pages/first_page.dart
+++ Flutter/demo/lib/pages/first_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'fifth_page.dart';
+import 'seventh_page.dart';
 
 class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,9 +11,28 @@ class FirstPage extends StatelessWidget {
         title: Text('First Page'),
         backgroundColor: Colors.blue, // Set the color for the AppBar
       ),
-      body: Center(
-        child: Text('Hello, this is the First Page!'),
-      ),
-    );
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FifthPage()),
+              );
+            },
+            child: Text('Go to Fifth Page'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SeventhPage()),
+              );
+            },
+            child: Text('Go to Seventh Page'),
+          ),
+        ]
+    ));
   }
 }
